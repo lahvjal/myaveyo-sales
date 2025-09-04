481ed85883ac3f3bf89007c65b15b8e40288ab6f
Lahvjal <lahvjalfuimaono@gmail.com>
2025-09-04 11:43:11 -0600

    Update OnTheInsideSection: replace custom play button with SVG icon and fix image source

--- components/OnTheInsideSection.tsx
+++ components/OnTheInsideSection.tsx
@@ -124,16 +124,12 @@ export default function OnTheInsideSection({ className = '', pageReady = true }:
             >
               <div className="relative h-[100%] bg-black rounded-[5px] overflow-hidden cursor-pointer group">
                 <img 
-                  src={imgCard}
+                  src="/images/consumer photos-29.jpeg"
                   alt="Video thumbnail"
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                 />
                 <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 group-hover:bg-opacity-20 transition-all duration-300">
-                  <div className="w-16 h-16 bg-white bg-opacity-90 rounded-full flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
-                    <svg className="w-6 h-6 text-black ml-1" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M8 5v14l11-7z"/>
-                    </svg>
-                  </div>
+                  <img src="/play.svg" alt="Play" className="w-10 h-10 ml-1" />
                 </div>
               </div>
             </div>
